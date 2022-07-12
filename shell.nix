d00fa433cd063ec9c460e0f2741f862b4a2855e7
Victor Engmark <vengmark@linz.govt.nz>
2022-07-12 16:15:20 +0200

    feat: Enable Nix shell

---
+++ shell.nix
@@ -0,0 +1,40 @@
+{ pkgs ? import
+    (
+      fetchTarball
+        {
+          name = "22.05";
+          url = "https://github.com/NixOS/nixpkgs/archive/ce6aa13369b667ac2542593170993504932eb836.tar.gz";
+          sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+        })
+    { }
+}:
+let
+  python = pkgs.python3;
+  projectDir = builtins.path {
+    path = ./.;
+    name = "oidc-provider";
+  };
+
+  poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+    inherit python projectDir;
+    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+      mypy = super.mypy.overridePythonAttrs (old: {
+        patches = [ ];
+        MYPY_USE_MYPYC = false;
+      });
+    });
+  };
+in
+poetryEnv.env.overrideAttrs (
+  oldAttrs: {
+    buildInputs = [
+      pkgs.cacert
+      pkgs.gitFull
+      pkgs.nodejs
+      (pkgs.poetry.override {
+        inherit python;
+      })
+      pkgs.which
+    ];
+  }
+)
